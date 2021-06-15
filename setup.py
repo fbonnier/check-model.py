b40263351c4b8eb0ea0f16caaedfdf7cfbf4c7e2
Florent B <75836984+fbonnier@users.noreply.github.com>
2021-06-15 12:20:24 +0200

    Create setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+rom setuptools import setup, find_packages
+
+
+install_requires = [
+    "hbp-validation-framework",
+]
+
+setup(
+    name='check-model',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/fbonnier/check-model.py.git',
+    license="CeCILL",
+    author='Florent Bonnier',
+    author_email='florent.bonnier@cnrs.fr',
+    description='EBRAINS Validation script generator',
+    # Requirements
+    install_requires=install_requires,
+)
