c9f0db735897d10564bbe9a0f490b343aee3c664
Florent B <75836984+fbonnier@users.noreply.github.com>
2021-06-29 17:24:42 +0200

    Delete code unzip call
    Code Unzip call is done during code location computation

--- check_model-kgv2.py
+++ check_model-kgv2.py
@@ -20,7 +20,6 @@ if __name__ == "__main__":
     print (type(model_instance.metadata["parameters"]))
     model_instance.create_script_file(work_dir)
     model_instance.write_code_location()
-    model_instance.write_code_unzip()
     model_instance.write_goto_project_folder()
     model_instance.write_pip_installs()
     model_instance.write_download_inputs()
