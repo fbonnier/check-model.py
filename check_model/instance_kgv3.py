6375a9a90e68aef99086d6975957f2afdd8029f4
Florent B <75836984+fbonnier@users.noreply.github.com>
2021-10-05 19:19:11 +0200

    Update KGV3

--- check_model/instance_kgv3.py
+++ check_model/instance_kgv3.py
@@ -12,15 +12,39 @@ from kg_core.kg import KGv3
 from kg_core.models import Stage
 from kg_core.models import Pagination
 
+# Source server from which the ALREADY KG-v3 instances are downloaded
+# Default is Official KG-v3 server
+SOURCE_SERVER = "core.kg.ebrains.eu"
+
 
 class KGV3_Instance (instance.Instance):
 
     def download_instance_metadata (self):
         print ("KGV3:: Download Instance")
         print ("KGV3 :: Get model instance metadata ==> START")
-        self.metadata = self.catalog.get_model_instance(instance_id=self.id)
-        self.metadata["parameters"] = json.loads(self.metadata["parameters"])
-        self.parse_html_options ()
+        kgv3_instance_metadata = self.catalog.get_instance(stage=Stage.IN_PROGRESS, instance_id=self.id).data()
+        print (kgv3_instance_metadata)
+        print ("\n\n")
+        # Get source repo
+        self.metadata["source"] = self.catalog.get_instance(stage=Stage.IN_PROGRESS, instance_id=kgv3_instance_metadata["https://openminds.ebrains.eu/vocab/repository"][0]["@id"].split("/")[-1]).data()["https://openminds.ebrains.eu/vocab/name"]
+        # self.metadata["inputs"] = self.catalog.get_instance(stage=Stage.IN_PROGRESS, instance_id=kgv3_instance_metadata["https://openminds.ebrains.eu/vocab/repository"][0]["@id"].split("/")[-1]).data()
+
+        # Try to get outputs
+        try:
+            self.metadata["outputs"] = [self.catalog.get_instance(stage=Stage.IN_PROGRESS, instance_id=item["@id"].split("/")[-1]).data()["https://openminds.ebrains.eu/vocab/IRI"] for item in kgv3_instance_metadata["https://openminds.ebrains.eu/vocab/outputData"]]
+        except KeyError as e:
+            print (e)
+            print_error("No ouputs, please choose of a model instance that provides outputs", "fail")
+            exit(EXIT_FAILURE)
+        print (self.metadata)
+        ## self.metadata = self.catalog.get_model_instance(instance_id=self.id)
+        ## self.metadata["parameters"] = json.loads(self.metadata["parameters"])
+        # self.parse_html_options ()
+
+        # Get parameters
+        # TODO
+
+
         print ("KGV3 :: Get model instance metadata ==> END")
 
     def create_script_file (self, work_dir):
@@ -80,7 +104,7 @@ class KGV3_Instance (instance.Instance):
 
     def connect_to_service (self, token):
         token_handler = SimpleToken(token)
-        return KGv3(host="core.kg.ebrains.eu", token_handler=token_handler)
+        return KGv3(host=SOURCE_SERVER, token_handler=token_handler)
 
     def __init__ (self, new_id, token):
         super().__init__ (new_id)
