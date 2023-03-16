22db153894ce596f708b9194299b8bb9b5a1d259
fbonnier <florent.bonnier@cnrs.fr>
2023-03-16 16:53:04 +0100

    Update test

--- tests.sh
+++ tests.sh
@@ -5,5 +5,6 @@
 # Model n°1: 79561fa3-093d-4d2b-b074-51af888eeb11
 
 MODEL_ID="79561fa3-093d-4d2b-b074-51af888eeb11"
+MAIN_DIR=$( dirname -- "$0"; )
 
-cwltool main.cwl --hbp_token $HBP_AUTH_TOKEN --instruction 'running instruction' --model_instance_id '79561fa3-093d-4d2b-b074-51af888eeb11' 
+cwltool $MAIN_DIR/main.cwl --hbp_token $HBP_AUTH_TOKEN --instruction 'running instruction' --model_instance_id '79561fa3-093d-4d2b-b074-51af888eeb11' 
